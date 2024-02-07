92cdd570c491d625dc77cf73cf1fe4fdf6dbea9a
3xg3lin <krmutku76@gmail.com>
2024-02-07 15:25:03 +0300

    Create test11.sh

---
+++ Using Structured Commands/test11.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Look before you leap
+#
+jump_directory=/home/arthur
+#
+if [ -d $jump_directory ]
+then
+    echo "The $jump_directory directory exists"
+    cd $jump_directory
+    ls
+else
+    echo "The $jump_directory directory does not exist"
+fi
+#
