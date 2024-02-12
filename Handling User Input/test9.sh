b26bf9e80cdd292d0bd1f668c1f7b98253799277
3xg3lin <krmutku76@gmail.com>
2024-02-12 13:45:38 +0300

    Create test9.sh

---
+++ Handling User Input/test9.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Testing parameters
+#
+if [ $# -ne 2 ]
+then
+    echo
+    echo Usage: test9.sh a b
+    echo
+else
+    total=$[ $1 + $2 ]
+    echo
+    echo The total is $total
+    echo
+fi
+#
